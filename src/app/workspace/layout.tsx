aee15ce1b101ebf9f6f995d5d0d4f6f5c04714c9
0xnald <chinaldo.zo@gmail.com>
2026-09-07 07:16:33 +0100

    feat: establish responsive Wardyn workspace and visual system

---
+++ src/app/workspace/layout.tsx
@@ -0,0 +1,3 @@
+import { Shell } from '../../components/shell';
+import { WorkspaceProvider } from '../../components/workspace-context';
+export default function Layout({ children }: { children: React.ReactNode }) { return <WorkspaceProvider><Shell>{children}</Shell></WorkspaceProvider>; }
